730f4205fd94f0d089bb1632e3c36b986c29e343
yuhaijun <hjyu@zhidu.xyz>
2021-07-22 01:14:12 +0800

    组件

---
+++ generalCrawler/lib/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/7/21 23:16
+# @Author  : Haijun
---
+++ generalCrawler/lib/base_fun.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/7/21 23:17
+# @Author  : Haijun
+
+import os
+from loguru import logger as base_logger
+
+
+def init_logger():
+    '''
+    日志
+    '''
+    base_logger.add(os.path.join('/data/logs', 'spider_info_{time:YYYY-MM-DD}.log'),
+                    format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {file.path} | {module} | {function} | {line} | {message}",
+                    level="INFO", rotation="00:00", retention='6 days', enqueue=True, encoding='utf-8')
+    base_logger.add(os.path.join('/data/logs', 'spider_error_{time:YYYY-MM-DD}.log'),
+                    format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {file.path} | {module} | {function} | {line} | {message}",
+                    level="ERROR", rotation="00:00", retention='6 days', enqueue=True, encoding='utf-8')
+    return base_logger
+
+
+logger = init_logger()
